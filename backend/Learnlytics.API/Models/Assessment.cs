3f4b1c46d875bdb38506c4ac5265175546b1e946
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-08-14 12:23:55 +0530

    Created Assessment and Attempt models

---
+++ backend/Learnlytics.API/Models/Assessment.cs
@@ -0,0 +1,63 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Learnlytics.API.Models
+{
+    public enum QuestionType
+    {
+        MCQ,
+        Coding
+    }
+
+    public class Assessment
+    {
+        [BsonId, BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string Title { get; set; } = null;
+        public string Description { get; set; } = null!;
+        public List<string> Skills { get; set; } = new();
+        public int DurationMinutes { get; set; } = 30;
+        public bool Published { get; set; } = false;
+
+        public List<QuestionBase> Questions { get; set; } = new();
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string CreatedBy { get; set; } = "";
+    }
+
+    [BsonDiscriminator(RootClass = true)]
+    [BsonKnownTypes(typeof(McqQuestion), typeof(CodingQuestion))]
+    public abstract class QuestionBase
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public QuestionType QuestionType { get; set; }
+        public string Prompt { get; set; } = null!;
+        public int Points { get; set; } = 1;
+    }
+
+    public class McqQuestion : QuestionBase
+    {
+        public List<string> Options { get; set; } = new();
+        public List<int> CorrectAnswers { get; set; } = new();
+        public bool ShuffleOptions { get; set; } = true;
+        public McqQuestion()
+        {
+            QuestionType = QuestionType.MCQ;
+        }
+    }
+
+    public class CodingQuestion : QuestionBase
+    {
+        public string? Language { get; set; } = "JavaScript";
+        public string? StarterCode { get; set; }
+        public List<string> TestCases { get; set; } = new();
+        public string? ExpectedOutput { get; set; }
+        public string PlagiarismGroupKey { get; set; } = "";
+        public CodingQuestion()
+        {
+            QuestionType = QuestionType.Coding;
+        }
+    }
+}
---
+++ backend/Learnlytics.API/Models/Attempt.cs
@@ -0,0 +1,64 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Learnlytics.API.Models
+{
+    public enum AttemptStatus
+    {
+        InProgress,
+        Submitted,
+        Expired
+    }
+
+    public class Attempt
+    {
+        [BsonId, BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? AssessmentId { get; set; } = null!;
+
+        public string Username { get; set; } = null!;
+        public DateTime StartedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? ExpiredAt { get; set; }
+        public DateTime? SubmittedAt { get; set; }
+        public AttemptStatus Status { get; set; } = AttemptStatus.InProgress;
+
+        public List<Answers> Answers { get; set; } = new();
+
+        public int AutoScore { get; set; } = 0;
+        public int ManualScore { get; set; } = 0;
+        public int TotalScore => AutoScore + ManualScore;
+    }
+
+    [BsonDiscriminator(RootClass = true)]
+    [BsonKnownTypes(typeof(McqAnswer), typeof(CodingAnswer))]
+    public abstract class Answers
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? QuestionId { get; set; } = null!;
+        public QuestionType QuestionType { get; set; }
+    }
+
+    public class McqAnswer : Answers
+    {
+        public List<int> SelectedOptions { get; set; } = new();
+        public bool IsCorrect { get; set; } = false;
+        public McqAnswer()
+        {
+            QuestionType = QuestionType.MCQ;
+        }
+    }
+
+    public class CodingAnswer : Answers
+    {
+        public string Code { get; set; }
+        public double? PlagiarismScore { get; set; }
+        public bool IsCorrect { get; set; } = false;
+        public List<string> SimilarAttemptIds { get; set; } = new();
+        public CodingAnswer()
+        {
+            QuestionType = QuestionType.Coding;
+        }
+    }
+}
