d016c4a3c420c8ac8813dda76e007fce332e6a06
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-08-14 17:14:47 +0530

    Create AttemptsController.cs

---
+++ backend/Learnlytics.API/Controllers/AttemptsController.cs
@@ -0,0 +1,43 @@
+﻿using Learnlytics.API.Models;
+using Learnlytics.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learnlytics.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AttemptsController : Controller
+    {
+        private readonly AttemptService _service;
+
+        public AttemptsController(AttemptService service) 
+        { 
+            _service = service;
+        }
+
+        [HttpPost("start")]
+        public async Task<IActionResult> Start([FromBody] StartAttemptDto startAttemptDto)
+        {
+            var username = User.Identity?.Name ?? throw new InvalidOperationException("No Identity");
+            var attempt = await _service.StartAttemptAsync(startAttemptDto.AssessmentId, username);
+            return Ok(attempt);
+        }
+
+        [HttpGet("{attemptId}")]
+        public async Task<IActionResult> Get(string attemptId)
+        {
+            var attempt = await _service.GetAttemptAsync(attemptId);
+            if (attempt == null)
+                return NotFound();
+            return Ok(attempt);
+        }
+
+        [HttpPost("submit")]
+        public async Task<IActionResult> Submit([FromBody] SubmitAttemptDto submitAttemptDto)
+        {
+            await _service.SubmitAnswerAsync(submitAttemptDto.AttemptId, submitAttemptDto.Answers);
+            var attempt = await _service.GetAttemptAsync(submitAttemptDto.AttemptId);
+            return Ok(new { attempt!.TotalScore, attempt.AutoScore, attempt.ManualScore, attempt.Status });
+        }
+    }
+}
