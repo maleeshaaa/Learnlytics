8eca5d80817b1e32f998da34b46157a7e03e5135
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-08-14 15:03:48 +0530

    Create DTOs.cs to define how data moves between the backend and the frontend

---
+++ backend/Learnlytics.API/Models/DTOs.cs
@@ -0,0 +1,47 @@
+﻿namespace Learnlytics.API.Models
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+
+    public class LoginDto
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+
+    public class RegisterDto
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string ConfirmPassword { get; set; } = null!;
+    }
+
+    public class CreateAssessmentDto
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = "";
+        public List<string> Skills { get; set; } = new();
+        public int DurationMinutes { get; set; } = 30;
+        public List<QuestionBase> Questions { get; set; } = new();
+    }
+
+    public class StartAttemptDto
+    {
+        public string AssessmentId { get; set; } = null!;
+    }
+
+    public class SubmitAttemptDto
+    {
+        public string AttemptId { get; set; } = null!;
+        public List<Answers> Answers { get; set; } = new();
+    }
+}
