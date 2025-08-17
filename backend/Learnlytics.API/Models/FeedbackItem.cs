95322ae17c219516ac1da1b2cc440cfb37afd449
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-08-17 11:38:23 +0530

    Create FeedbackItem.cs

---
+++ backend/Learnlytics.API/Models/FeedbackItem.cs
@@ -0,0 +1,8 @@
+namespace Learnlytics.API.Models
+{
+    public class FeedbackItem
+    {
+        public string QuestionId { get; set; } = string.Empty;
+        public string Feedback { get; set; } = string.Empty;
+    }
+}
